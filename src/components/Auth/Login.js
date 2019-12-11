e9592fc8df4acbf8d26e3923b4ae092a08aed12b
Yotam <yotam.hakim@gmail.com>
2019-12-11 14:59:51 +0200

    User Sessions with useAuth Hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -27,7 +27,8 @@ function Login(props) {
     try {
       login
         ? await firebase.login(email, password)
-        : await firebase.register(name, email, password)
+        : await firebase.register(name, email, password);
+      props.history.push('/');
     } catch (err) {
       console.error('Authentication Error', err);
       setFirebaseError(err.message);
