0a1893e8a751bfc4c1574fd2ee8e4d86a5e7c397
Yotam <yotam.hakim@gmail.com>
2019-12-11 11:41:22 +0200

    Validation to useFormValidation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import useFormValidation from './useFormValidation';
+import validateLogin from './validateLogin';
 
 const INITIAL_STATE = {
   name: "",
@@ -10,7 +11,10 @@ const INITIAL_STATE = {
 
 
 function Login(props) {
-  const { handleSubmit, handleChange, values } = useFormValidation(INITIAL_STATE);
+  const { handleSubmit, handleBlur, handleChange, values, errors, isSubmitting } = useFormValidation(
+    INITIAL_STATE,
+    validateLogin
+  );
   const [login, setLogin] = React.useState(true);
 
   return (
@@ -28,21 +32,29 @@ function Login(props) {
           />}
         <input
           onChange={handleChange}
+          onBlur={handleBlur}
           value={values.email}
           name="email"
           type="email"
+          className={errors.email && 'error-input'}
           placeholder="Your email"
           autoComplete="off"
         />
+        {errors.email && <p className="error-text">{errors.email}</p>}
         <input
           onChange={handleChange}
+          onBlur={handleBlur}
           value={values.password}
           name="password"
           type="password"
+          className={errors.password && 'error-input'}
           placeholder="Choose a secure password"
         />
+        {errors.password && <p className="error-text">{errors.password}</p>}
         <div className="flex mt3">
-          <button type="submit" className="button pointer mr2">
+          <button type="submit" className="button pointer mr2" disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
             Submit
           </button>
           <button type="button" className="button pointer"
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function useFormValidation(initialState) {
+function useFormValidation(initialState, validate) {
     const [values, setValues] = React.useState(initialState);
+    const [errors, setErrors] = React.useState({});
+    const [isSubmitting, setSubmitting] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isSubmitting) {
+            const noErrors = Object.keys(errors).length === 0;
+
+            if (noErrors) {
+                console.log('authenticated', values);
+                setSubmitting(false);
+            } else {
+                setSubmitting(false);
+            }
+        }
+    }, [errors])
 
     function handleChange(event) {
         event.persist();
@@ -11,12 +26,20 @@ function useFormValidation(initialState) {
         }));
     }
 
+    function handleBlur() {
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+        setSubmitting(true);
         console.log({ values })
     }
 
-    return { handleSubmit, handleChange, values }
+    return { handleSubmit, handleBlur, handleChange, values, errors, isSubmitting }
 }
 
 export default useFormValidation;
